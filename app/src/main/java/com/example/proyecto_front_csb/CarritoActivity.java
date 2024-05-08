d689b66e860457efe563054c1291a21511a6b998
cathy_sulbaran <cathysulbaran@gmail.com>
2024-05-08 21:48:54 +0200

    intento de generar compra (no finalizado)

--- app/src/main/java/com/example/proyecto_front_csb/CarritoActivity.java
+++ app/src/main/java/com/example/proyecto_front_csb/CarritoActivity.java
@@ -1,7 +1,5 @@
 package com.example.proyecto_front_csb;
 
-import static com.example.proyecto_front_csb.model.Productos.DocumentSnapshot;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,19 +13,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
 import com.example.proyecto_front_csb.model.Productos;
 
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
@@ -40,6 +38,7 @@ public class CarritoActivity extends AppCompatActivity {
     private Adaptador_ventas Adaptador_ventas;
     private TextView txtResumen;
     private Button btConfirmarCompra;
+    private ArrayList<Productos> productosSeleccionados = new ArrayList<>();
 
     EditText usuario, telefono, direccion;
     private Document pdfDocument;
@@ -61,7 +60,7 @@ public class CarritoActivity extends AppCompatActivity {
         txtResumen = findViewById(R.id.txtResumen);
 
         // Obtener la lista de productos seleccionados del intent
-        ArrayList<Productos> productosSeleccionados = getIntent().getParcelableArrayListExtra("productosSeleccionados");
+        productosSeleccionados = getIntent().getParcelableArrayListExtra("productosSeleccionados");
 
         // Configurar el adaptador para el RecyclerView
         Adaptador_ventas = new Adaptador_ventas(productosSeleccionados);
@@ -75,38 +74,41 @@ public class CarritoActivity extends AppCompatActivity {
         btConfirmarCompra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ArrayList<Productos> productos = new ArrayList<>();
-                final int[] documentosCompletos = {0}; // Contador para el número de documentos obtenidos
 
-                for (Productos producto : Adaptador_ventas.getProductosSeleccionados()) {
-                    // Suponiendo que tienes acceso a la referencia de la colección de Firestore donde están los productos
-                    // Utiliza el método get() para obtener los objetos Productos para cada producto
-                    // Añade estos productos a la lista
-                    db.document(producto.getEan()).get()
-                            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                                @Override
-                                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                    Productos productoObtenido = documentSnapshot.toObject(Productos.class);
-                                    productos.add(productoObtenido);
-                                    documentosCompletos[0]++; // Incrementa el contador de documentos obtenidos
+                final int totalProductos = productosSeleccionados.size(); // Obtener el número total de productos
+                final List<Productos> productosObtenidos = new ArrayList<>(); // Lista para almacenar los productos obtenidos
 
-                                    // Cuando todos los documentos hayan sido agregados, genera el informe
-                                    if (documentosCompletos[0] == Adaptador_ventas.getProductosSeleccionados().size()) {
-                                        generarInformeCompra(productos);
+                // Iterar sobre los productos seleccionados
+                for (Productos producto : productosSeleccionados) {
+                    String ean = producto.getEan();
+                    if (ean != null && !ean.isEmpty()) {
+                        FirebaseFirestore.getInstance().collection("Productos")
+                                .document(ean)
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            DocumentSnapshot document = task.getResult();
+                                            if (document.exists()) {
+                                                // Convertir el documento en un objeto Productos
+                                                Productos producto = document.toObject(Productos.class);
+                                                // Agregar el producto a la lista de productos obtenidos
+                                                productosObtenidos.add(producto);
+                                            }
+                                        }
+
+                                        // Verificar si se han obtenido todos los productos
+                                        if (productosObtenidos.size() == totalProductos) {
+                                            // Una vez que se han obtenido todos los productos, generar el PDF
+                                            generarPDF(productosObtenidos);
+                                        }
                                     }
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    // Manejar errores de lectura de Firestore aquí
-                                }
-                            });
+                                });
+                    }
                 }
             }
         });
-
-
 
 
     }
@@ -125,9 +127,10 @@ public class CarritoActivity extends AppCompatActivity {
         txtResumen.setText(resumen.toString());
     }
 
-    private void generarInformeCompra(ArrayList<Productos> productos) {
+
+    public void generarPDF(List<Productos> productos) {
         // Especifica la ruta de guardado del archivo PDF
-        String fileName = "Compra_informe.pdf";
+        String fileName = "Informe_Productos.pdf";
         File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
         File file = new File(directory, fileName);
 
@@ -138,24 +141,18 @@ public class CarritoActivity extends AppCompatActivity {
             Document pdfDocument = new Document(pdf);
 
             // Agregar contenido al documento
-            pdfDocument.add(new Paragraph("Informe de Compra\n\n"));
-
-            obtenerDatosUsuario();
+            pdfDocument.add(new Paragraph("Listado de Productos\n\n"));
 
             // Agregar los detalles de cada producto al documento
             for (Productos producto : productos) {
-                // Utiliza el ID del documento como el campo "ean"
-                String nombre = producto.getNombre();
-                String fichaTecnica = producto.getFichaTecnica();
-                String marca = producto.getMarca();
-                double precio = producto.getPrecio();
-                int unidades = producto.getUnidades();
+                String ean = producto.getEan(); // Obtenemos el EAN del producto
+                String nombre = producto.getNombre(); // Obtenemos el nombre del producto
+                double precio = producto.getPrecio(); // Obtenemos el precio del producto
+                int unidades = producto.getUnidades(); // Obtenemos la cantidad de unidades del producto
 
-                // Agregar los detalles del producto al documento
+                pdfDocument.add(new Paragraph("EAN: " + ean));
                 pdfDocument.add(new Paragraph("Nombre: " + nombre));
-                pdfDocument.add(new Paragraph("Marca: " + marca));
                 pdfDocument.add(new Paragraph("Precio: " + precio));
-                pdfDocument.add(new Paragraph("Ficha Técnica: " + fichaTecnica));
                 pdfDocument.add(new Paragraph("Unidades: " + unidades));
                 pdfDocument.add(new Paragraph("\n")); // Separador entre productos
             }
@@ -167,9 +164,12 @@ public class CarritoActivity extends AppCompatActivity {
             Toast.makeText(this, "Informe generado correctamente en: " + file.getParentFile(), Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             e.printStackTrace();
-            Toast.makeText(this, "Error al guardar el informe de compra", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Error al guardar el informe de productos", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
 
     private void obtenerDatosUsuario() {
 
